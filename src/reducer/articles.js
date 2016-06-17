50a1c777bea9e48eb43d2e26f89f985a4189e0be
Roman <kaboom.ua@gmail.com>
2016-06-16 20:29:48 -0700

    Update articles.js

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -7,8 +7,10 @@ export default (articles = normalizedArticles, action) => {
     switch (type) {
         case DELETE_ARTICLE:
         	return articles.filter(article => article.id != payload.id);
+        	//во первых, это лишнее - вы таже можете реагировать на ADD_COMMENT
         case UPDATE_ARTICLE:
         	return articles.map(article => {
+        			//во вторых, вы тут попробовали избавиться от мутации, но comments[payload.comments.length] = ... изменяет массив article.comments 
 				return article.id == payload.id ? {...article, ...article.comments[payload.comments.length] = payload.comments.length} : article;
 			})
     }
